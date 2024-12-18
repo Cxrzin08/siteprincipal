778f5f8295306df0783851f7475d9d2f2362528c
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-12-17 23:16:35 -0300

    Atualizar o manter_ativos.py

--- manter_ativos.py
+++ manter_ativos.py
@@ -9,7 +9,7 @@ def manter_sites_ativos():
     while True:
         for site in sites:
             requests.get(site, timeout=10)
-            time.sleep(300)
+            time.sleep(200)
 
 if __name__ == "__main__":
     manter_sites_ativos()
